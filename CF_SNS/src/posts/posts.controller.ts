0e9fd9a8427851d857d8175eb420b72aac4f2798
kimproweb101 <kimproweb101@gmail.com>
2025-09-11 17:59:49 +0900

    Get Posts 구현하기

--- CF_SNS/src/posts/posts.controller.ts
+++ CF_SNS/src/posts/posts.controller.ts
@@ -1,7 +1,8 @@
 import { Controller, Get } from '@nestjs/common';
 import { PostsService } from './posts.service';
 
-interface Post {
+interface PostModel {
+  id: number;
   author: string;
   title: string;
   content: string;
@@ -9,17 +10,38 @@ interface Post {
   commentCount: number;
 }
 
+const posts: PostModel[] = [
+  {
+    id: 1,
+    author: 'author01',
+    title: 'title01',
+    content: 'content01',
+    likeCount: 100,
+    commentCount: 100,
+  },
+  {
+    id: 2,
+    author: 'author02',
+    title: 'title02',
+    content: 'content02',
+    likeCount: 100,
+    commentCount: 100,
+  },
+  {
+    id: 3,
+    author: 'author03',
+    title: 'title03',
+    content: 'content03',
+    likeCount: 100,
+    commentCount: 100,
+  },
+];
+
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
   @Get()
-  getPost(): Post {
-    return {
-      author: 'newjeans_official',
-      title: '뉴진스 민지',
-      content: '메이크업 고치고 있는 민지',
-      likeCount: 10000000,
-      commentCount: 999999,
-    };
+  getPosts() {
+    return posts;
   }
 }
--- node.md
+++ node.md
@@ -1,3 +1,16 @@
 19. Post 모듈 생성하기
 nest cli 로 posts 생성
 nest g resourse
+
+20. Postman을 사용해서 요청 보내기
+
+21. REST API 세트
+GET POST PATCH PUT DELETE 설명
+PATCH 8이라는 ID가 존재할때만 작동
+PUT : 8이라는 ID가 있으면 변경 없으면 생성, 여차하면 생성해야할수도있어서 모든 데이터를 넣어준다
+DELETE
+
+QUERY GET 
+BODY POST PATCH PUT DELETE
+
+22. Get Post 구현하기
