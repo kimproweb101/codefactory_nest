4d55811f923e2e140b55d1da2f884d7606e82b8a
kimproweb101 <kimproweb101@gmail.com>
2025-09-11 16:05:29 +0900

    11. Path 별로 다른 응답 변환하기

--- 1_server.js
+++ 1_server.js
@@ -1,10 +1,24 @@
 const http=require('http');
 const host='localhost';
 const port=3000;
+const url=require('url')
 
 const server=http.createServer((req,res)=>{
-  res.writeHead(200,{'Content-type':'text-html'});
-  res.end('<h1>hello world</h1>')
+  const path=url.parse(req.url).pathname
+
+  if(path==='/'){
+    res.writeHead(200, {'Content-type':'text-html'})
+    res.end('<h1>Home Page!</h1>')
+  }else if(path==='/post'){
+    res.writeHead(200, {'Content-type':'text-html'})
+    res.end('<h1>Post Page!</h1>')
+  }else if(path==='/user'){
+    res.writeHead(200, {'Content-type':'text-html'})
+    res.end('<h1>User Page!</h1>')
+  }else{
+    res.writeHead(404, {'Content-type':'text-html'})
+    res.end('<h1>404 Page not found</h1>')   
+  }
 })
 
 server.listen(port, host, ()=>{
