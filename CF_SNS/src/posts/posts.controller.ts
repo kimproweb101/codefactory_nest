3abb555f957fdc612411e50e57136af44171c9b4
kimproweb101 <kimproweb101@gmail.com>
2025-09-11 18:07:55 +0900

    ID Param을 이용해서 하나의 Post 가져오기

--- CF_SNS/src/posts/posts.controller.ts
+++ CF_SNS/src/posts/posts.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { PostsService } from './posts.service';
 
 interface PostModel {
@@ -43,5 +43,10 @@ export class PostsController {
   @Get()
   getPosts() {
     return posts;
+  }
+
+  @Get(':id')
+  getPost(@Param('id') id: string) {
+    return posts.find((post) => post.id === +id);
   }
 }
