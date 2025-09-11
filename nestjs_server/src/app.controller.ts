f7cd3161a0a0aafbf902c8bb1f2be1d786e54fd6
kimproweb101 <kimproweb101@gmail.com>
2025-09-11 16:38:38 +0900

    13.Nest Hello World 해보기

--- nestjs_server/src/app.controller.ts
+++ nestjs_server/src/app.controller.ts
@@ -6,7 +6,17 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHome(): string {
+    return "Home Page"
+  }
+
+  @Get('post')
+  getPost(){
+    return 'Post Page'
+  }
+
+  @Get('user')
+  getUser(){
+    return 'User Page'
   }
 }
