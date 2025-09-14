b4ef33a0ffdec73176d2a8cdb128fce84b70bebd
kimproweb <kimproweb101@gmail.com>
2025-09-14 16:09:31 +0900

    헤더 값으로부터 토큰 추출하는 로직 작성하기

--- CF_SNS/src/auth/auth.service.ts
+++ CF_SNS/src/auth/auth.service.ts
@@ -97,4 +97,15 @@ export class AuthService {
     });
     return this.loginUser(newUser);
   }
+
+  extractTokenFromHeader(header: string, isBearer: boolean) {
+    const splitToken = header.split(' ');
+    const prefix = isBearer ? 'Bearer' : 'Basic';
+    if (splitToken.length !== 2 || splitToken[0] !== prefix) {
+      throw new UnauthorizedException('잘못된 토큰입니다.');
+    }
+    const token = splitToken[1];
+
+    return token;
+  }
 }
