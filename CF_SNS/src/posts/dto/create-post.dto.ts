17f0484a0cabd011f4069a01566d4b38b3cb3c09
kimproweb <kimproweb101@gmail.com>
2025-09-18 01:16:58 +0900

    ClassValidator 에러 메시지 변경하기

--- CF_SNS/src/posts/dto/create-post.dto.ts
+++ CF_SNS/src/posts/dto/create-post.dto.ts
@@ -1,9 +1,13 @@
 import { IsString } from 'class-validator';
 
 export class CreaetePostDto {
-  @IsString()
+  @IsString({
+    message: 'title은 string 타입을 입력 해야 합니다.',
+  })
   title: string;
 
-  @IsString()
+  @IsString({
+    message: 'content string 타입을 입력 해야 합니다.',
+  })
   content: string;
 }
